0573e01c07c5cf35c8d9fec031790b41044c2617
h0r0man <roman.horokhovatskyy@gmail.com>
2017-05-15 18:05:03 +0200

    Changed home thumbnail size

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -7,7 +7,7 @@
     </div>
     <?php if ( has_post_thumbnail() ): ?>
       <div class="hero-image">
-        <?php the_post_thumbnail(); ?>
+        <?php the_post_thumbnail('large'); ?>
       </div>
     <?php endif; ?>
   </header>
