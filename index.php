fe7a18c269493bd4fda3b928eefcec81d07aa917
h0r0man <roman.horokhovatskyy@gmail.com>
2017-05-16 13:47:45 +0200

    Added index markup

--- index.php
+++ index.php
@@ -2,27 +2,61 @@
 
 <?php if (have_posts()): ?>
 
-  <section class="index">
+  <article>
 
-    <?php if (is_category()): ?>
-      <h2><?php single_cat_title(); ?></h2>
-    <?php elseif (is_tag()): ?>
-      <h2><?php single_tag_title(); ?></h2>
-    <?php elseif (is_search()): ?>
-      <h2><?php printf( __( 'Search Results for: %s', 'gieffe-videogames' ), get_search_query() ); ?></h2>
-    <?php else: ?>
-      <h2><?php esc_html_e('News', 'gieffe-videogames'); ?></h2>
+    <header class="hero">
+      <div class="hero-content">
+        <?php if ( is_category() ) : ?>
+          <h2><?php single_cat_title(); ?></h2>
+        <?php elseif ( is_tag() ) : ?>
+          <h2><?php single_tag_title(); ?></h2>
+        <?php elseif ( is_search() ) : ?>
+          <h2><?php printf( __('Search Results for: %s', 'gieffe-videogames'), get_search_query() ); ?></h2>
+        <?php else: ?>
+          <h2><?php esc_html_e('News', 'gieffe-videogames'); ?></h2>
+        <?php endif; ?>
+        <?php if ( get_field('index_hero_description', get_option('page_for_posts')) ) : ?>
+          <p><?php the_field('index_hero_description', get_option('page_for_posts')) ?></p>
+        <?php endif; ?>
+        <section class="hero-categories">
+          <h3 class="hidden"><?php esc_html_e('News categories', 'gieffe-videogames'); ?></h3>
+          <ul>
+            <?php
+              $categories = get_categories();
+              foreach ($categories as $category) {
+                echo '<li><a href="' . get_category_link($category->term_id) . '" rel="bookmark">' . $category->name . '</a></li>';
+              }
+            ?>
+          </ul>
+        </section>
+      </div>
+    </header>
+
+    <div class="posts">
+      <?php while ( have_posts() ) : the_post(); ?>
+        <?php get_template_part('template-parts/content', 'preview'); ?>
+      <?php endwhile; ?>
+    </div>
+
+    <?php if ( paginate_links() ) : ?>
+      <nav class="pagination">
+        <h4><?php esc_html_e('Pagination', 'gieffe-videogames'); ?></h4>
+        <?php
+          $options = array(
+          	'prev_next'          => true,
+          	'prev_text'          => __('« Previous', 'gieffe-videogames'),
+          	'next_text'          => __('Next »', 'gieffe-videogames'),
+          	'type'               => 'list'
+          );
+          echo paginate_links( $options );
+        ?>
+      </nav>
     <?php endif; ?>
-          
-    <?php while (have_posts()): the_post(); ?>
-      <?php get_template_part('template-parts/content', 'preview'); ?>
-    <?php endwhile; ?>
 
-  </section>
-        
+  </article>
+
 <?php else: ?>
   <?php get_template_part('template-parts/content', 'none'); ?>
 <?php endif; ?>
-
 
 <?php get_footer(); ?>
--- template-parts/content-preview.php
+++ template-parts/content-preview.php
@@ -1,7 +1,22 @@
-<article class="preview">
+<article class="posts-item">
   <a rel="bookmark" href="<?php the_permalink(); ?>">
-    <?php the_post_thumbnail(); ?>
+    <?php the_post_thumbnail('medium'); ?>
     <h3><?php the_title(); ?></h3>
     <?php the_excerpt(); ?>
+    <div class="posts-meta">
+      <?php
+        $timeSemantic = get_the_time('c');
+        $timePlain = get_the_date();
+      ?>
+      <time datetime="<?php echo $timeSemantic; ?>"><?php echo $timePlain; ?></time>
+      <ul>
+        <?php
+          $categories = get_the_category();
+          foreach ($categories as $category) {
+            echo '<li>' . $category->name . '</li>';
+          }
+        ?>
+      </ul>
+    </div>
   </a>
 </article>
