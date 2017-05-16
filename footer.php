dca48b6c5eecf0b9fc6eaec35e78adf1571d84a3
h0r0man <roman.horokhovatskyy@gmail.com>
2017-05-16 12:53:47 +0200

    Improved footer CTA semantics

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <?php if ( get_field('options_cta_title', option) && get_field('options_cta_description', option) && get_field('options_cta_link_1', option) && get_field('options_cta_link_1_name', option) && get_field('options_cta_link_2', option) && get_field('options_cta_link_2_name', option) ): ?>
-      <section class="cta">
+      <aside class="cta">
         <div class="cta-content">
           <h4><?php the_field('options_cta_title', option); ?></h4>
           <p><?php the_field('options_cta_description', option); ?></p>
@@ -8,13 +8,11 @@
             <a href="mailto:<?php the_field('options_cta_link_2', option); ?>" rel="bookmark" class="button"><?php the_field('options_cta_link_2_name', option); ?></a>
           </div>
         </div>
-      </section>
+      </aside>
     <?php endif; ?>
-
     <footer role="contentinfo" class="footer">
       <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> – <?php bloginfo('description'); ?>.</p>
     </footer>
-
     <?php wp_footer(); ?>
   </body>
 </html>
