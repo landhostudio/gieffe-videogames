66e96f8d31486e122039f99880e068251fbd55e0
h0r0man <roman.horokhovatskyy@gmail.com>
2017-05-16 15:57:24 +0200

    Make template part a CTA

--- footer.php
+++ footer.php
@@ -1,15 +1,4 @@
-    <?php if ( get_field('options_cta_title', option) && get_field('options_cta_description', option) && get_field('options_cta_link_1', option) && get_field('options_cta_link_1_name', option) && get_field('options_cta_link_2', option) && get_field('options_cta_link_2_name', option) ): ?>
-      <aside class="cta">
-        <div class="cta-content">
-          <h4><?php the_field('options_cta_title', option); ?></h4>
-          <p><?php the_field('options_cta_description', option); ?></p>
-          <div class="button-group">
-            <a href="mailto:<?php the_field('options_cta_link_1', option); ?>" rel="bookmark" class="button"><?php the_field('options_cta_link_1_name', option); ?></a>
-            <a href="mailto:<?php the_field('options_cta_link_2', option); ?>" rel="bookmark" class="button"><?php the_field('options_cta_link_2_name', option); ?></a>
-          </div>
-        </div>
-      </aside>
-    <?php endif; ?>
+    <?php get_template_part('template-parts/content', 'cta'); ?>
     <footer role="contentinfo" class="footer">
       <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> – <?php bloginfo('description'); ?>.</p>
     </footer>
---
+++ template-parts/content-cta.php
@@ -0,0 +1,12 @@
+<?php if ( get_field('options_cta_title', option) && get_field('options_cta_description', option) && get_field('options_cta_link_1', option) && get_field('options_cta_link_1_name', option) && get_field('options_cta_link_2', option) && get_field('options_cta_link_2_name', option) ): ?>
+  <aside class="cta">
+    <div class="cta-content">
+      <h4><?php the_field('options_cta_title', option); ?></h4>
+      <p><?php the_field('options_cta_description', option); ?></p>
+      <div class="button-group">
+        <a href="mailto:<?php the_field('options_cta_link_1', option); ?>" rel="bookmark" class="button"><?php the_field('options_cta_link_1_name', option); ?></a>
+        <a href="mailto:<?php the_field('options_cta_link_2', option); ?>" rel="bookmark" class="button"><?php the_field('options_cta_link_2_name', option); ?></a>
+      </div>
+    </div>
+  </aside>
+<?php endif; ?>
