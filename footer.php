4fc2f9df30b40ebb0fac13762e4d6cb04b2d00b1
h0r0man <roman.horokhovatskyy@gmail.com>
2017-05-16 16:14:54 +0200

    Added statement to be ignored on contact template

--- footer.php
+++ footer.php
@@ -1,4 +1,6 @@
-    <?php get_template_part('template-parts/content', 'cta'); ?>
+    <?php if ( !is_page_template('template-pages/contact.php') ) : ?>
+      <?php get_template_part('template-parts/content', 'cta'); ?>
+    <?php endif; ?>
     <footer role="contentinfo" class="footer">
       <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> – <?php bloginfo('description'); ?>.</p>
     </footer>
