9b706fa068857dd8cd828b6e69de82fb098df63d
h0r0man <roman.horokhovatskyy@gmail.com>
2017-05-23 15:41:12 +0200

    Reodered post hero content

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -1,15 +1,18 @@
 <article>
   
   <header class="hero">
-    <div class="hero-content">
-      <h2><?php the_title(); ?></h2>
-      <?php get_template_part('template-parts/content', 'meta'); ?>
-    </div>
+
     <?php if ( has_post_thumbnail() ): ?>
       <div class="hero-image">
         <?php the_post_thumbnail('large'); ?>
       </div>
     <?php endif; ?>
+
+    <div class="hero-content">
+      <h2><?php the_title(); ?></h2>
+      <?php get_template_part('template-parts/content', 'meta'); ?>
+    </div>
+
   </header>
 
   <div class="content">
