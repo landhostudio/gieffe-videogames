60c109fedf490b317988cf4f817d7714fc572bc8
h0r0man <roman.horokhovatskyy@gmail.com>
2017-05-23 13:28:09 +0200

    Restyled Gaming Hall template
    Removed Google Maps

--- template-parts/content-gaming-hall.php
+++ template-parts/content-gaming-hall.php
@@ -1,41 +1,56 @@
 <article>
 
-  <?php if ( get_field('gaming_hall_hero_title') && get_field('gaming_hall_hero_description') || has_post_thumbnail() ) : ?>
-    <header class="hero">
+  <header class="hero">
+
+    <?php
+      $images = get_field( 'gaming_hall_carousel' );
+      if ( $images ) :
+    ?>
+      <div class="hero-carousel">
+        <ul>
+          <?php
+            foreach ( $images as $image ) :
+            $image = $image[ID];
+          ?>
+            <li>
+              <?php echo wp_get_attachment_image( $image, 'large', false, array() ); ?>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
+    <?php elseif ( has_post_thumbnail() ): ?>
+      <div class="hero-image">
+        <?php the_post_thumbnail( 'large' ); ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ( get_field('gaming_hall_hero_title') && get_field('gaming_hall_hero_description') ) : ?>
       <div class="hero-content">
         <h2><?php the_field('gaming_hall_hero_title'); ?></h2>
         <p><?php the_field('gaming_hall_hero_description') ?></p>
       </div>
-      <?php if ( has_post_thumbnail() ): ?>
-        <div class="hero-image">
-          <?php the_post_thumbnail('large'); ?>
-        </div>
-      <?php endif; ?>
-    </header>
-  <?php endif; ?>
+    <?php endif; ?>
 
-  <?php if ( have_rows('gaming_hall_map') ) : ?>
-    <div class="map">
-      <div class="map-canvas">
-        <?php
-          while ( have_rows('gaming_hall_map') ) : the_row();
-          $location = get_sub_field('gaming_hall_location');
-        ?>
-          <div class="map-canvas-location" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>">
-            <h3><?php the_sub_field('gaming_hall_location_name'); ?></h3>
-            <?php the_sub_field('gaming_hall_location_description'); ?>
-          </div>
-        <?php endwhile; ?>
-      </div>
-    </div>
-  <?php endif; ?>
+  </header>
 
-  <?php if ( have_rows('gaming_hall_map') ) : ?>
+  <?php if ( have_rows('gaming_hall_locations') ) : ?>
     <div class="locations">
-      <?php while ( have_rows('gaming_hall_map') ) : the_row(); ?>
+      <?php while ( have_rows('gaming_hall_locations') ) : the_row(); ?>
         <article class="locations-item">
-          <h3><?php the_sub_field('gaming_hall_location_name'); ?></h3>
-          <?php the_sub_field('gaming_hall_location_description'); ?>
+
+          <?php if ( get_sub_field( 'gaming_hall_location_image' ) ): ?>
+            <div class="location-image">
+              <?php echo wp_get_attachment_image(get_sub_field('gaming_hall_location_image'), 'medium', false, array()); ?>
+            </div>
+          <?php endif; ?>
+
+          <?php if ( get_sub_field( 'gaming_hall_location_name' ) && get_sub_field( 'gaming_hall_location_description' ) ): ?>
+            <div class="location-content">
+              <h3><?php the_sub_field('gaming_hall_location_name'); ?></h3>
+              <?php the_sub_field('gaming_hall_location_description'); ?>
+            </div>
+          <?php endif; ?>
+
         </article>
       <?php endwhile; ?>
     </div>
