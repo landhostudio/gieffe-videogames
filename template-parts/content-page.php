d9ee4a8765d901356a8872d6d45a39d811878e8e
h0r0man <roman.horokhovatskyy@gmail.com>
2017-05-23 17:55:37 +0200

    Added main default template

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,5 +1,21 @@
-<article class="page">
-  <?php the_post_thumbnail(); ?>
-  <h2><?php the_title(); ?></h2>
-  <?php the_content(); ?>
+<article>
+
+  <header class="hero">
+
+    <?php if ( has_post_thumbnail() ): ?>
+      <div class="hero-image">
+        <?php the_post_thumbnail( 'large' ); ?>
+      </div>
+    <?php endif; ?>
+
+    <div class="hero-content">
+      <h2><?php the_title(); ?></h2>
+    </div>
+
+  </header>
+
+  <div class="content">
+    <?php the_content(); ?>
+  </div>
+
 </article>
