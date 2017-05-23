3137737bb6e669092e9d10c0dd063f9e6f965963
h0r0man <roman.horokhovatskyy@gmail.com>
2017-05-23 15:51:41 +0200

    Added carousel to about template

--- template-parts/content-about.php
+++ template-parts/content-about.php
@@ -1,18 +1,37 @@
 <article>
 
-  <?php if ( get_field('about_hero_title') && get_field('about_hero_description') || has_post_thumbnail() ) : ?>
-    <header class="hero">
+  <header class="hero">
+
+    <?php
+      $images = get_field( 'about_carousel' );
+      if ( $images ) :
+    ?>
+      <div class="hero-carousel">
+        <ul>
+          <?php
+            foreach ( $images as $image ) :
+            $image = $image[ID];
+          ?>
+            <li>
+              <?php echo wp_get_attachment_image( $image, 'large', false, array() ); ?>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
+    <?php elseif ( has_post_thumbnail() ): ?>
+      <div class="hero-image">
+        <?php the_post_thumbnail( 'large' ); ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ( get_field('about_hero_title') && get_field('about_hero_description') ) : ?>
       <div class="hero-content">
         <h2><?php the_field('about_hero_title'); ?></h2>
         <p><?php the_field('about_hero_description') ?></p>
       </div>
-      <?php if ( has_post_thumbnail() ): ?>
-        <div class="hero-image">
-          <?php the_post_thumbnail('large'); ?>
-        </div>
-      <?php endif; ?>
-    </header>
-  <?php endif; ?>
+    <?php endif; ?>
+
+  </header>
 
   <div class="content">
     <?php the_content(); ?>
