3e230d6c2abdee983c31005300b007a643d46542
h0r0man <roman.horokhovatskyy@gmail.com>
2017-05-23 12:49:29 +0200

    Added home carousel

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -1,23 +1,38 @@
 <article>
 
-  <?php if ( get_field('home_hero_title') && get_field('home_hero_description') && get_field('home_hero_link') && get_field('home_hero_link_name') || has_post_thumbnail() ) : ?>
-    <header class="hero">
-      <div class="hero-content">
-        <h2><?php the_field('home_hero_title'); ?></h2>
-        <p><?php the_field('home_hero_description') ?></p>
-        <a href="<?php the_field('home_hero_link'); ?>" rel="bookmark" class="button"><?php the_field('home_hero_link_name'); ?></a>
-      </div>
-      <?php if ( has_post_thumbnail() ): ?>
-        <div class="hero-image">
-          <?php the_post_thumbnail('large'); ?>
-        </div>
-      <?php endif; ?>
-    </header>
-  <?php endif; ?>
+  <header class="hero">
 
-  <?php
-    // TODO: featured product categories
-  ?>
+    <?php
+      $images = get_field( 'home_carousel' );
+      if ( $images ) :
+    ?>
+      <div class="hero-carousel">
+        <ul>
+          <?php
+            foreach ( $images as $image ) :
+            $image = $image[ID];
+          ?>
+            <li>
+              <?php echo wp_get_attachment_image( $image, 'large', false, array() ); ?>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
+    <?php elseif ( has_post_thumbnail() ): ?>
+      <div class="hero-image">
+        <?php the_post_thumbnail( 'large' ); ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ( get_field( 'home_hero_title' ) && get_field( 'home_hero_description' ) && get_field( 'home_hero_link' ) && get_field( 'home_hero_link_name' ) ) : ?>
+      <div class="hero-content">
+        <h2><?php the_field( 'home_hero_title' ); ?></h2>
+        <p><?php the_field( 'home_hero_description' ) ?></p>
+        <a href="<?php the_field( 'home_hero_link' ); ?>" rel="bookmark" class="button"><?php the_field( 'home_hero_link_name' ); ?></a>
+      </div>
+    <?php endif; ?>
+
+  </header>
 
   <?php if ( have_rows('home_sections') ): ?>
     <?php while ( have_rows('home_sections') ) : the_row(); ?>
