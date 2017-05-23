aa125e1cab81f527afe9088a357a8c1ca688891a
h0r0man <roman.horokhovatskyy@gmail.com>
2017-05-23 13:04:35 +0200

    Added carousel to vr page

--- template-parts/content-vr.php
+++ template-parts/content-vr.php
@@ -1,18 +1,37 @@
 <article>
+  
+  <header class="hero">
 
-  <?php if ( get_field('vr_hero_title') && get_field('vr_hero_description') || has_post_thumbnail() ) : ?>
-    <header class="hero">
+    <?php
+      $images = get_field( 'vr_carousel' );
+      if ( $images ) :
+    ?>
+      <div class="hero-carousel">
+        <ul>
+          <?php
+            foreach ( $images as $image ) :
+            $image = $image[ID];
+          ?>
+            <li>
+              <?php echo wp_get_attachment_image( $image, 'large', false, array() ); ?>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
+    <?php elseif ( has_post_thumbnail() ): ?>
+      <div class="hero-image">
+        <?php the_post_thumbnail( 'large' ); ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ( get_field('vr_hero_title') && get_field('vr_hero_description') ) : ?>
       <div class="hero-content">
         <h2><?php the_field('vr_hero_title'); ?></h2>
         <p><?php the_field('vr_hero_description') ?></p>
       </div>
-      <?php if ( has_post_thumbnail() ): ?>
-        <div class="hero-image">
-          <?php the_post_thumbnail('large'); ?>
-        </div>
-      <?php endif; ?>
-    </header>
-  <?php endif; ?>
+    <?php endif; ?>
+
+  </header>
 
   <?php if ( have_rows('vr_sections') ): ?>
     <?php while ( have_rows('vr_sections') ) : the_row(); ?>
