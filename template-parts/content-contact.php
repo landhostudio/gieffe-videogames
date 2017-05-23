fd184f8bcf8b998ea386127788328f6442f001ac
h0r0man <roman.horokhovatskyy@gmail.com>
2017-05-23 17:50:23 +0200

    Added Google Map to contact template
    Also added carousel

--- template-parts/content-contact.php
+++ template-parts/content-contact.php
@@ -1,22 +1,48 @@
 <article>
 
-  <?php if ( get_field('contact_hero_title') && get_field('contact_hero_description') || has_post_thumbnail() ) : ?>
-    <header class="hero">
-      <div class="hero-content">
-        <h2><?php the_field('contact_hero_title'); ?></h2>
-        <p><?php the_field('contact_hero_description') ?></p>
+  <header class="hero">
+
+    <?php
+      $images = get_field( 'contact_carousel' );
+      if ( $images ) :
+    ?>
+      <div class="hero-carousel">
+        <ul>
+          <?php
+            foreach ( $images as $image ) :
+            $image = $image[ID];
+          ?>
+            <li>
+              <?php echo wp_get_attachment_image( $image, 'large', false, array() ); ?>
+            </li>
+          <?php endforeach; ?>
+        </ul>
       </div>
-      <?php if ( has_post_thumbnail() ): ?>
-        <div class="hero-image">
-          <?php the_post_thumbnail('large'); ?>
-        </div>
-      <?php endif; ?>
-    </header>
-  <?php endif; ?>
+    <?php elseif ( has_post_thumbnail() ): ?>
+      <div class="hero-image">
+        <?php the_post_thumbnail( 'large' ); ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ( get_field( 'contact_hero_title' ) && get_field( 'contact_hero_description' ) ) : ?>
+      <div class="hero-content">
+        <h2><?php the_field( 'contact_hero_title' ); ?></h2>
+        <p><?php the_field( 'contact_hero_description' ) ?></p>
+      </div>
+    <?php endif; ?>
+
+  </header>
 
   <?php
-    // TODO: contact form
+    $location = get_field( 'contact_map' );
+    if ( !empty( $location ) ) :
   ?>
+    <div class="map">
+      <div class="map-canvas">
+        <div class="map-location" data-lat="<?php echo $location[ 'lat' ]; ?>" data-lng="<?php echo $location[ 'lng' ]; ?>"></div>
+      </div>
+    </div>
+  <?php endif; ?>
 
   <div class="content">
     <?php the_content(); ?>
