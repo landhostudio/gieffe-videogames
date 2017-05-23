1a0f3086cf9b21bfe83c09bfa1ea83877f20a60c
h0r0man <roman.horokhovatskyy@gmail.com>
2017-05-23 12:00:38 +0200

    Funtions code polishing

--- functions.php
+++ functions.php
@@ -1,16 +1,16 @@
 <?php
 
-  if (!function_exists('gieffe_videogames_setup')) {
+  if ( !function_exists( 'gieffe_videogames_setup' ) ) {
 
     function gieffe_videogames_setup() {
       
       // Let WordPress manage the document title -------------------------------
 			
-      add_theme_support('title-tag');
+      add_theme_support( 'title-tag' );
 
 			// Enable support for Post Thumbnails on posts and pages -----------------
 			
-      add_theme_support('post-thumbnails');
+      add_theme_support( 'post-thumbnails' );
 
 			// Enables dynamic navigation --------------------------------------------
 
@@ -21,14 +21,18 @@
       // Load the assets -------------------------------------------------------
 			
 			function init_assets() {
-        wp_enqueue_style('css-main', get_template_directory_uri() . '/dist/css/main.css', array(), '1.0.0');
-        wp_enqueue_script('js-plugins', get_template_directory_uri() . '/dist/js/vendor.js', array('jquery'), '1.0.0', true);
-        if (is_page_template('template-pages/gaming-hall.php')) {
-          wp_enqueue_script('google-maps-api', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAy9L-ARM6uJGEjfALrS5RBVuPYlD8nfRc', array(), '1.0.0', true);
+
+        wp_enqueue_style( 'css-main', get_template_directory_uri() . '/dist/css/main.css', array(), '1.0.0' );
+        wp_enqueue_script( 'js-plugins', get_template_directory_uri() . '/dist/js/vendor.js', array( 'jquery' ), '1.0.0', true );
+
+        if ( is_page_template( 'template-pages/gaming-hall.php' ) ) {
+          wp_enqueue_script( 'google-maps-api', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAy9L-ARM6uJGEjfALrS5RBVuPYlD8nfRc', array(), '1.0.0', true);
         }
-        wp_enqueue_script('js-scripts', get_template_directory_uri() . '/dist/js/main.js', array('jquery'), '1.0.0', true);
+
+        wp_enqueue_script( 'js-scripts', get_template_directory_uri() . '/dist/js/main.js', array( 'jquery' ), '1.0.0', true );
+
 			}
-			add_action('wp_enqueue_scripts', 'init_assets');
+			add_action( 'wp_enqueue_scripts', 'init_assets' );
       
       // Content Width ---------------------------------------------------------
       
@@ -36,27 +40,27 @@
 
       // Soil ------------------------------------------------------------------
       
-      get_template_part('inc/soil');
+      get_template_part( 'inc/soil' );
 
       // ACF -------------------------------------------------------------------
 
-      if (function_exists('acf_add_options_page')) {
+      if ( function_exists( 'acf_add_options_page' ) ) {
 
-        acf_add_options_page(array(
-      		'page_title' 	=> 'Options',
-      		'menu_title'	=> 'Options',
-      		'menu_slug' 	=> 'theme-options'
+        acf_add_options_page( array(
+      		'page_title' => 'Options',
+      		'menu_title' => 'Options',
+      		'menu_slug'  => 'theme-options'
       	));
         
       }
 
-      function my_acf_google_map_api($api) {
+      function my_acf_google_map_api( $api ) {
         $api['key'] = 'AIzaSyAy9L-ARM6uJGEjfALrS5RBVuPYlD8nfRc';
         return $api;
       }
-      add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
+      add_filter( 'acf/fields/google_map/api', 'my_acf_google_map_api' );
       
     }
 
   }
-  add_action('after_setup_theme', 'gieffe_videogames_setup');
+  add_action( 'after_setup_theme', 'gieffe_videogames_setup' );
